c1bd273812be30138a1fb30a8515e6a862a8af7c
luis030794 <luis.tobar.94@hotmail.com>
2015-12-20 21:41:24 -0500

    Base/usuario
    cambio en los modelos y base

--- apps/seguridad/models.py
+++ apps/seguridad/models.py
@@ -2,10 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
 
-class usuarios(models.Model):
-	name= models.CharField(max_length=10)
-	last_name=models.CharField(max_length=10)
-	date_create=models.DateTimeField('fecha de creacion')
-	password=models.CharField(max_length=6)
+# Create your models here.
+#class usuarios(models.Model):
+##	name= models.CharField(max_length=10)
+#	last_name=models.CharField(max_length=10)
+#	date_create=models.DateTimeField('fecha de creacion')
+#	password=models.CharField(max_length=6)##
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(upload_to='profiles', blank=True, null=True)
