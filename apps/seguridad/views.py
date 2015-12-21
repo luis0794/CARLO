511ac121538a5e9fde704cf70ef7f7373c16a745
luis030794 <luis.tobar.94@hotmail.com>
2015-12-20 23:22:10 -0500

    s
    d

--- apps/seguridad/views.py
+++ apps/seguridad/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, RequestContext, render_to_response
+from django.shortcuts import render, RequestContext, render_to_response, redirect
 from django.http import request, HttpResponse, HttpResponseRedirect, Http404
 from .forms import RegistroUserForm
+
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import reverse
+
+from .forms import RegistroUserForm
+from .models import UserProfile
 
 
 import time
@@ -30,12 +37,26 @@ def usuarios(request):
 def ingresarusuario(request):
 	if request.method == 'POST':
 		form = RegistroUserForm(request.POST, request.FILES)
-	else:
-		form = RegistroUserForm()
-	context = {
-		'form': form
-	}
-	return render_to_response('ingresarusuario.html',context=RequestContext(request))
+		if form.is_valid():
+			cleaned_data = form.cleaned_data
+			username = cleaned_data.get('username')
+			password = cleaned_data.get('password')
+			photo = cleaned_data.get('photo')
+			user_model = User.objects.create_user(username=username, password=password)
+			user_model.save()
+			user_profile = UserProfile()
+			user_profile.user = user_model
+			user_profile.photo = photo
+			user_profile.save()
+			return redirect(reverse('usuarios.html', kwargs={'username': username}))
+		else:
+			form = RegistroUserForm()
+		context = {'form': form}
+		return render(request, 'usuarios.html', context)
+
+
+def gracias_view(request, username):
+    return render(request, 'usuarios', {'username': username})
 
 def modificarcontrasena(request):
     return render_to_response('modificarcontrasena.html',context=RequestContext(request))  
